faeec4fb4fa4d98521b143370f09e009e9b11d02
andrey.abmyetko <andrey.abmyetko@ice.com>
2021-04-12 16:12:02 +0300

    implemented @RegisterExtension static ScreenShooterExtension

--- src/test/java/Test_Base.java
+++ src/test/java/Test_Base.java
@@ -1,10 +1,18 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.junit5.ScreenShooterExtension;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 /* https://selenide.org/javadoc/current/com/codeborne/selenide/Configuration.html */
 
+@ExtendWith({ScreenShooterExtension.class})
 public class Test_Base {
+
+    @RegisterExtension
+    static ScreenShooterExtension screenshotEmAll = new ScreenShooterExtension(true).to("target/screenshots");
 
     @BeforeAll
     static void setUp(){
