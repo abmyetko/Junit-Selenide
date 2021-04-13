fd931651f764bdfc8e8b1b9446ff6b42b320e628
andrey.abmyetko <andrey.abmyetko@ice.com>
2021-04-13 10:55:15 +0300

    implemented @TestMethodOrder(MethodOrderer.OrderAnnotation.class) - to run tests in specific order

--- src/test/java/Test_StartScreen.java
+++ src/test/java/Test_StartScreen.java
@@ -1,38 +1,47 @@
 import com.ice.screens.StartScreen;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class Test_StartScreen extends Test_Base{
 
     public static final StartScreen startScreen = open("/", StartScreen.class);
 
     @Test
+    @Order(1)
     public void checkAboutIsOpen(){
         startScreen.About.click();
         startScreen.Learn_more_about_ICE.shouldBe(visible);
     }
 
     @Test
+    @Order(2)
     public void checkSolutionsIsOpen(){
         startScreen.Solutions.click();
         startScreen.Featured_Services.shouldBe(visible);
     }
 
     @Test
+    @Order(3)
     public void checkResourcesIsOpen(){
         startScreen.Resources.click();
         startScreen.ICE_Product_Guide.shouldBe(visible);
     }
 
     @Test
+    @Order(4)
     public void checkInsightsIsOpen(){
         startScreen.Insights.click();
         startScreen.Market_Pulse.shouldBe(visible);
     }
 
     @Test
+    @Order(5)
     public void checkContactsIsOpen(){
         startScreen.Contact.click();
     }
